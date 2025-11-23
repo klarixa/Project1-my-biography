f058d5975ed21bf3ebd4c39d1a5e410cb0d05ccc
Ramesh <klarixaramesh@gmail.com>
2025-11-23 13:24:08 +0800

    Initial commit

---
+++ project-01-biography/src/lib/Footer.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let name;
+</script>
+
+<p class="bg-info text-white text-center m-5 p-3">Copyright@ 2025 {name}. All Rights Reserved.</p>
---
+++ project-01-biography/src/lib/Header.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let title;
+</script>
+
+<h1 class="bg-info text-white text-center m-5 p-3">{title}</h1>
